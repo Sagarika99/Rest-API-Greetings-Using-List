d0803166e6118b2aae98dcbfea0390edfb6b0efd
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-09-25 18:04:27 +0530

    returned simple greeting message

--- src/main/java/com/practice/greetingslist/controller/GreetingController.java
+++ src/main/java/com/practice/greetingslist/controller/GreetingController.java
@@ -3,7 +3,9 @@ package com.practice.greetingslist.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.practice.greetingslist.model.Greetings;
@@ -20,4 +22,9 @@ public class GreetingController {
 						);
 	}
 	
+	@GetMapping("/hello")
+	public String sayHello(@RequestParam(value = "firstName") String firstName,
+			@RequestParam(value = "lastName") String lastName) {
+		return "Hello " + firstName + " " + lastName + "!";
+	}
 }
