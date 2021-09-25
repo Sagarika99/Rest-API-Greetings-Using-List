4c8a5d3d16211e9494ead5a9d8a3fccb00538060
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-09-25 16:25:13 +0530

    First Commit

---
+++ src/main/java/com/practice/greetingslist/GreetingslistApplication.java
@@ -0,0 +1,13 @@
+package com.practice.greetingslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingslistApplication.class, args);
+	}
+
+}
